4bc70979d52630e42741b169245bd49dc9d54d13
Andrea Sandvik Hansen <70581941+ansanhan@users.noreply.github.com>
2021-08-31 14:14:29 +0200

    oppg 1_2 1 og 2 uke35

---
+++ src/uke35/Uke35Oppg2_12345.java
@@ -0,0 +1,117 @@
+package uke35;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Uke35Oppg2_12345 {
+    public static void main(String[] args)      // hovedprogram
+    {
+        int[] a = randPerm(20);              // en tilfeldig tabell
+        for (int k : a) System.out.print(k + " ");  // skriver ut a
+
+        int m = maks(a);   // finner posisjonen til største verdi
+
+        System.out.println("\nStørste verdi ligger på plass " + m);
+
+    }
+
+    private Uke35Oppg2_12345() {}   // privat standardkonstruktør - hindrer instansiering
+
+    // Metoden bytt(int[] a, int i, int j)       Programkode 1.1.8 d)
+    public static void bytt(int[] a, int i, int j)
+    {
+        int temp = a[i]; a[i] = a[j]; a[j] = temp;
+    }
+
+
+    // Metoden randPerm(int n)                   Programkode 1.1.8 e)
+    public static int[] randPerm(int n)  // en effektiv versjon
+    {
+        Random r = new Random();         // en randomgenerator
+        int[] a = new int[n];            // en tabell med plass til n tall
+
+        Arrays.setAll(a, i -> i + 1);    // legger inn tallene 1, 2, . , n
+
+        for (int k = n - 1; k > 0; k--)  // løkke som går n - 1 ganger
+        {
+            int i = r.nextInt(k+1);        // en tilfeldig tall fra 0 til k
+            bytt(a,k,i);                   // bytter om
+        }
+
+        return a;                        // permutasjonen returneres
+    }
+
+
+    // Metoden randPerm(int[] a)                 Programkode 1.1.8 f)
+    public static void randPerm(int[] a)  // stokker om a
+    {
+        Random r = new Random();     // en randomgenerator
+
+        for (int k = a.length - 1; k > 0; k--)
+        {
+            int i = r.nextInt(k + 1);  // tilfeldig tall fra [0,k]
+            bytt(a,k,i);
+        }
+    }
+
+
+    // Metoden maks(int[] a, int fra, int til)   Programkode 1.2.1 b)
+    public static int maks(int[] a, int fra, int til)
+    {
+        if (fra < 0 || til > a.length || fra >= til)
+        {
+            throw new IllegalArgumentException("Illegalt intervall!");
+        }
+
+        int m = fra;              // indeks til største verdi i a[fra:til>
+        int maksverdi = a[fra];   // største verdi i a[fra:til>
+
+        for (int i = fra + 1; i < til; i++)
+        {
+            if (a[i] > maksverdi)
+            {
+                m = i;                // indeks til største verdi oppdateres
+                maksverdi = a[m];     // største verdi oppdateres
+            }
+        }
+
+        return m;  // posisjonen til største verdi i a[fra:til>
+    }
+
+
+    // Metoden maks(int[] a)                     Programkode 1.2.1 c)
+    public static int maks(int[] a)  // bruker hele tabellen
+    {
+        return maks(a,0,a.length);     // kaller metoden over
+    }
+
+
+    // min-metodene - se Oppgave 1 i Avsnitt 1.2.1
+    public static int min(int[] a, int fra, int til)
+    {
+        if (fra < 0 || til > a.length || fra >= til)
+        {
+            throw new IllegalArgumentException("Illegalt intervall!");
+        }
+
+        int m = fra;              // indeks til minste verdi i a[fra:til>
+        int minverdi = a[fra];   // minste verdi i a[fra:til>
+
+        for (int i = fra + 1; i < til; i++)
+        {
+            if (a[i] < minverdi)
+            {
+                m = i;                // indeks til minste verdi oppdateres
+                minverdi = a[m];     // minste verdi oppdateres
+            }
+        }
+
+        return m;  // posisjonen til minste verdi i a[fra:til>
+    }
+
+
+    public static int min(int[] a)  // bruker hele tabellen
+    {
+        return min(a,0,a.length);     // kaller metoden over
+    }
+}
